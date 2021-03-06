2842f17fa7cefff899ef05c257c2d4867860b73e
eddie <etan72@student.monash.edu>
2021-03-06 21:37:47 +1100

    popup links work but,,,,
    background colour isnt workin out

---
+++ src/pages/popup.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Link, navigate } from "gatsby";
+import 'fontsource-poppins';
+import './button.css';
+import './popup.css';
+import rec from "./../images/images_png/recieve_req.png";
+import x_but from "./../images/images_png/x_but.png";
+
+export default function popup() {
+    return (
+        <div id='mainBar2'  style={{
+            backgroundColor: '#EFF0F6',
+          }}>
+            <img src={rec} width='120%' id='rec' ></img>
+            <button id='red' onClick={() => navigate('/request-help')}>
+                Update Status
+            </button>
+            <img src={x_but} width='20%' id='x' onClick={() => navigate('/')}></img>
+        </div>
+    )
+}
