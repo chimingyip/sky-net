c5de19aecafea76a85e543f1f872c6b286cf32f5
eddie <etan72@student.monash.edu>
2021-03-06 19:09:32 +1100

    ye looks like messsed ngl
    pls check marcus/tim/chi

--- src/pages/request-help.js
+++ src/pages/request-help.js
@@ -1,10 +1,35 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
+import 'fontsource-poppins';
+import './button.css';
+import './request.css';
+import name from "./../images/images_png/name.png";
+import loc from "./../images/images_png/loc.png";
+import stat from "./../images/images_png/status.png";
+import audio from "./../images/images_png/send_audio.png";
 
 export default function RequestHelp() {
     return (
+        <div id='mainBar'>
+            <h1 id='header'>Submit Request</h1>
+            <img src={name} width='100%' id='name' ></img>
+            <img src={loc} width='100%' id='name' ></img>
+            <img src={stat} width='100%' id='name' ></img>
+            <img src={audio} width='100%' id='name' ></img>
+
+            <button id='firstst' onClick={() => navigate('/')}>
+                back
+                </button>
+            {/* <Link to="/">HOME</Link> */}
+        </div>
+    )
+}
+
+
+/*
+return (
         <div>
             <Link to="/">HOME</Link>
         </div>
     )
-}
+*/
