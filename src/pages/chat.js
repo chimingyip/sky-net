93fe78d55618d0cfdfe71b9c37fe9b77deb14d0b
chimingyip <cmyip99@gmail.com>
2021-03-06 12:54:54 +1100

    Added new pages

---
+++ src/pages/chat.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export default function Chat() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Link to="/">HOME</Link>
+        </div>
+    )
+}
---
+++ src/pages/information-feed.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+export default function InformationFeed() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Link to="/">HOME</Link>
+        </div>
+    )
+}
