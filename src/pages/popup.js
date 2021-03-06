5ab2772b98ab88d15d895985dc1e1a5e2449590b
Marcus Lim <marcus4lim@gmail.com>
2021-03-06 22:21:55 +1100

    Fixed color changes

--- src/pages/popup.js
+++ src/pages/popup.js
@@ -8,16 +8,17 @@ import x_but from "./../images/images_png/x_but.png";
 import { useEffect } from "react";
 
 export default function Popup() {
-    useEffect(() => { document.body.style.backgroundColor = '#EFF0F6' }, [])
     return (
-        <div id='mainBar2'  style={{
-            backgroundColor: '#EFF0F6',
-          }}>
-            <img src={rec} width='120%' id='rec' ></img>
-            <button id='red' onClick={() => navigate('/request-help')}>
-                Update Status
-            </button>
-            <img src={x_but} width='20%' id='x' onClick={() => navigate('/')}></img>
+        <div style={{ backgroundColor: '#EFF0F6', height: '100%', width: '100%'}}>
+            <div id='mainBar2'  style={{
+                backgroundColor: '#EFF0F6',
+            }}>
+                <img src={rec} width='120%' id='rec' ></img>
+                <button id='red' onClick={() => navigate('/request-help')}>
+                    Update Status
+                </button>
+                <img src={x_but} width='20%' id='x' onClick={() => navigate('/')}></img>
+            </div>
         </div>
     )
 }
