0f503aaba1b2d6a6b882dd8e72c46722033198e4
Marcus Lim <marcus4lim@gmail.com>
2021-03-06 16:28:30 +1100

    first page done
    should look fine on desktop ratios

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,25 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { navigate } from "gatsby";
 import 'fontsource-poppins';
 import './button.css';
-
+import logo from "./../images/skynet.png";
 
 const homePage = () => {
     return (
-        <main style={{ width: '100%', height: '90%', margin: 0, padding: 0, top: 0 }}>
+        <main style={{ width: '100%', height: '100%', margin: 0, padding: 0, top: 0 }}>
             <title>Sky-net</title>
             <div id='mainBar'>
                 {/* <h1>Hello World</h1> */}
-                <button id='first'>
+                <button id='first' onClick={() => navigate('information-feed')}>
                     OFFICAL INFORMATION
                 </button>
-                <button id='second'>
+                <button id='second' onClick={() => navigate('request-help')}>
                     REQUEST HELP
                 </button>
-                <button id='third'>
+                <button id='third' onClick={() => navigate('chat')}>
                     CHAT WITH EMERGENCY SERVICES
                 </button>
+                <img src={logo} width='50%' style={{ marginTop: 'auto'}}/>
                 {/* <p style={{margin: 0}}>hello</p> */}
             </div>
         </main>
@@ -27,22 +28,3 @@ const homePage = () => {
 
 
 export default homePage;
-
-const mainBarStyles = {
-    backgroundColor: 'rgba(255, 0, 0, 0.3)',
-    margin: '2% auto',
-    maxWidth: '50em',
-    width: '60%',
-    minHeight: 500,
-    height: '100%',
-}
-const buttonStyles = {
-    width: '100%',
-    fontFamily: 'Poppins',
-    marginVertical: 50,
-    borderRadius: 18,
-    fontWeight: 'Bold',
-    fontSize: 36,
-    color: 'white'
-    
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,30 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { navigate } from "gatsby";
+import 'fontsource-poppins';
+import './button.css';
+import logo from "./../images/skynet.png";
 
-// markup
-export default function Home() {
+const homePage = () => {
   return (
-    <main style={{ width: '100%', height: '90%', margin: 0, padding: 0, top: 0 }}>
-            <title>Sky-net</title>
-            <div id='mainBar'>
-                {/* <h1>Hello World</h1> */}
-                <button id='first'>
-                    OFFICAL INFORMATION
+    <main style={{ width: '100%', height: '100%', margin: 0, padding: 0, top: 0 }}>
+      <title>Sky-net</title>
+      <div id='mainBar'>
+        {/* <h1>Hello World</h1> */}
+        <button id='first' onClick={() => navigate('information-feed')}>
+          OFFICAL INFORMATION
                 </button>
-                <button id='second'>
-                    REQUEST HELP
+        <button id='second' onClick={() => navigate('request-help')}>
+          REQUEST HELP
                 </button>
-                <button id='third'>
-                    CHAT WITH EMERGENCY SERVICES
+        <button id='third' onClick={() => navigate('chat')}>
+          CHAT WITH EMERGENCY SERVICES
                 </button>
-                {/* <p style={{margin: 0}}>hello</p> */}
-            </div>
-        </main>
+        <img src={logo} width='50%' style={{ marginTop: 'auto' }} />
+        {/* <p style={{margin: 0}}>hello</p> */}
+      </div>
+    </main>
   )
 }
+
+
+export default homePage;
