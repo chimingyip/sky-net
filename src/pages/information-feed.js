e7a74bafbdc46a8236b13e93064ad2818e0c513f
Marcus Lim <marcus4lim@gmail.com>
2021-03-06 18:00:33 +1100

    Made small changes,
    Removed scrolling

--- src/pages/information-feed.js
+++ src/pages/information-feed.js
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import 'fontsource-poppins';
+import './button.css';
+import logo from "./../images/skynet.png";
 
 export default function InformationFeed() {
     return (
-        <div>
+        <div id='mainBar'>
             <h1>Hello World</h1>
             <Link to="/">HOME</Link>
         </div>
